df36ac244a725ee5ed7277f795f7a7fe5831f71b
Pete Leaning <leaning@leaning.worksdesign.loc>
2011-11-23 16:21:43 -0500

    help page added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ MicroBlog::Application.routes.draw do
   get "pages/contact"
   
   get "pages/about"
+  
+  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
