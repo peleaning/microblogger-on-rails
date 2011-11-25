9728b7101d9ca5cd769c47af023b202f76470d3d
Pete Leaning <leaning@leaning.worksdesign.loc>
2011-11-25 13:31:21 -0500

    followers, follower feeds, guide content completed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,6 +6,22 @@ class Micropost < ActiveRecord::Base
 	validates :content, :presence => true, :length => { :maximum => 140}
 	validates :user_id, :presence => true
 	
-	
+	#def self.from_users_followed_by(user)
+	#	where(:user_id => user.following.push(user))
+	#end
+		
 	default_scope :order => 'microposts.created_at DESC'
+	
+	scope :from_users_followed_by, lambda { |user| followed_by(user) }
+
+
+	
+private	
+
+	def self.followed_by(user)
+		following_ids = %(SELECT followed_id FROM relationships
+		 				  WHERE follower_id = :user_id)
+		where("user_id IN (#{following_ids}) OR user_id = :user_id",
+			  { :user_id => user})
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	
 	has_many :microposts, :dependent => :destroy
+	has_many :relationships, :foreign_key => "follower_id",
+							 :dependent   => :destroy
+	
+	has_many :following, :through => :relationships, :source => :followed
+	
+	has_many :reverse_relationships, :foreign_key => "followed_id",
+									 :class_name  => "Relationship",
+									 :dependent   => :destroy
+	has_many :followers, :through => :reverse_relationships, :source => :follower
 	
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
 	
@@ -16,6 +25,7 @@ class User < ActiveRecord::Base
 						 :length       => { :within => 6..40 }
 						 
 	before_save :encrypt_password					 
+	
 	
 	def has_password?(submitted_password)
 		encrypted_password == encrypt(submitted_password)
@@ -32,8 +42,20 @@ class User < ActiveRecord::Base
 		(user && user.salt == cookie_salt) ? user :nil
 	end
 	
+	def following?(followed)
+		relationships.find_by_followed_id(followed)
+	end
+	
+	def follow!(followed)
+		relationships.create!(:followed_id => followed.id)
+	end
+	
+	def unfollow!(followed)
+		relationships.find_by_followed_id(followed).destroy
+	end
+	
 	def feed
-		Micropost.where("user_id = ?", id)
+		Micropost.from_users_followed_by(self)
 	end
 	
 	private
