19d7cc18f2d252f2d91ac13d79b07bbe68f51d15
Pete Leaning <leaning@leaning.worksdesign.loc>
2011-11-28 13:19:43 -0500

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ MicroBlog::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
 
